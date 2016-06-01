1e89c1822b3e378d106c709cf25b2c8ac3311eeb
Taaylored <tprince09@eagles.ewu.edu>
2016-06-01 14:45:22 -0700

    rotating gallery

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 // My Scripts
-$('.card').lightGallery();
+$(".cards").lightGallery({
+    selector: '.item'
+});
 
 $('#custom-easing').lightGallery({
     cssEasing: 'cubic-bezier(0.600, 0.040, 0.980, 0.335)'
